670728ca21f4a647e99aa5016fe8de5d59f1918f
Anderson-Oloroso <henrikog4@gmail.com>
2026-06-06 16:10:00 -0600

    :sparkles: Feat(ejerici0-03): Ejercicio arrays y tuplas terminado

--- ejercicios/03-arrays-y-tuplas/index.ts
+++ ejercicios/03-arrays-y-tuplas/index.ts
@@ -5,3 +5,23 @@ export {};
 
 // TODO: crea un array de jugadores.
 // TODO: crea una tupla [golesLocal, golesVisitante].
+
+console.log("========== Arrays y Tuplas ==========");
+
+let jugadores: string[] = [
+  "Messi",
+  "Ronaldo",
+  "Neymar",
+  "Mbappe",
+  "Lewandowski",
+];
+let resultadoPartido: [number, number] = [3, 2];
+
+console.log("Jugadores del equipo:");
+jugadores.forEach((jugador, index) => {
+  console.log(`${index + 1}. ${jugador}`);
+});
+
+console.log(
+  `Resultado del partido: ${resultadoPartido[0]} - ${resultadoPartido[1]}`,
+);
