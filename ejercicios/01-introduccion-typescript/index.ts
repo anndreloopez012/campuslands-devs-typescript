9887be83d77d9ffafec48c82c098363434a0e124
lemical07 <icaleladio@gmail.com>
2026-06-07 15:04:05 -0600

    feat: agregar ejercicio introductorio de TypeScript para jugador de shooter
    Conocer la sintáxis básica de TypeScript y ejecutar un archivo tipado.

--- ejercicios/01-introduccion-typescript/index.ts
+++ ejercicios/01-introduccion-typescript/index.ts
@@ -6,3 +6,14 @@ export {};
 // TODO: declara las variables tipadas del jugador.
 
 // TODO: imprime un resumen usando console.log.
+
+const nombreJugador: string = "PlayerOne";
+let armaPrincipal: string= "Rifle de Asalto";
+let municion: number = 120;
+let escudo: boolean = true;
+const mapa: string = "Desierto de Arena";
+const modoJuego: string = "Battle Royale";
+
+const resumen: string =
+    nombreJugador + " está jugando " + modoJuego + " en el mapa " + mapa + " con el arma " + armaPrincipal + " y tiene " + municion + " balas y escudo " + (escudo ? "activo" : "inactivo") + ".";
+console.log(resumen);
