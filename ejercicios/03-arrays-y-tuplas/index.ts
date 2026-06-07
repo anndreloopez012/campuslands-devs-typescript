ecf90e39581074228aee125ebc9ddf673951b475
DulceMR17 <adulcemoralesr@gmail.com>
2026-06-07 15:38:11 -0600

    feat: resolucion del ejercicio 03 arreglos y tuplas

--- ejercicios/03-arrays-y-tuplas/index.ts
+++ ejercicios/03-arrays-y-tuplas/index.ts
@@ -4,4 +4,23 @@ export {};
 // Contexto: Equipos de futbol
 
 // TODO: crea un array de jugadores.
+const jugadores: string[] = ["Pedro", "Luis", "Marcos", "Simone", "Eduardo"];
+const dorsales: number[] = [10, 7, 9, 11, 8];
+
 // TODO: crea una tupla [golesLocal, golesVisitante].
+const marcador: [number, number] = [5, 9];
+
+// Crea un nuevo arreglo combinando los dorsales y los nombres de los jugadores
+const nomina: string[] = dorsales.map(
+  (dorsal, index) => `Jugador: ${jugadores[index]} - Camiseta: #${dorsal}`,
+);
+
+// Cálculo de goles totales en el partido
+const golesTotales: number = marcador[0] + marcador[1];
+
+// Imprime la nómina del equipo, el marcador actual y el total de goles en el partido.
+console.log("Nómina del equipo:\n", nomina.join("\n"));
+console.log(
+  `\nMarcador actual:\nLocal ${marcador[0]} - Visitante ${marcador[1]}\n`,
+);
+console.log(`Total de goles en el partido: ${golesTotales}`);
