f442838a0bb08c9067d71920f8fd6e7801814ca3
Anderson-Oloroso <henrikog4@gmail.com>
2026-06-06 20:56:22 -0600

    :sparkles: Feat(ejercicio-06): Funciones tipadas

--- ejercicios/06-funciones-tipadas/index.ts
+++ ejercicios/06-funciones-tipadas/index.ts
@@ -4,3 +4,14 @@ export {};
 // Contexto: Sistema de puntuacion deportiva
 
 // TODO: crea funciones para calcular puntos.
+
+function calcularPuntos(goles: number, asistencias: number): number {
+  const puntosPorGol = 4;
+  const puntosPorAsistencia = 3;
+  return goles * puntosPorGol + asistencias * puntosPorAsistencia;
+}
+
+const goles = 10;
+const asistencias = 5;
+const puntosTotales = calcularPuntos(goles, asistencias);
+console.log(`Puntos totales: ${puntosTotales}`);
