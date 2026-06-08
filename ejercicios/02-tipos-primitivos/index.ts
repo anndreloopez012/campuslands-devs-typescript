67f97c8c682a1668b5908f4f1dc16ab09157a6e3
lemical07 <icaleladio@gmail.com>
2026-06-07 20:49:32 -0600

    feat: modelar datos de moto y validar mantenimiento por kilometraje

--- ejercicios/02-tipos-primitivos/index.ts
+++ ejercicios/02-tipos-primitivos/index.ts
@@ -6,3 +6,19 @@ export {};
 // TODO: declara datos primitivos para una moto.
 
 // TODO: calcula si necesita mantenimiento.
+
+let marca: string= "pulsar";
+let cilindrada: number = 150;
+let estaEncendida: boolean = false;
+let kilometraje: number = 3500;
+let proximoMantenimientoKm: number | null = null;  
+let necesitaMantenimiento: boolean = kilometraje > 3000;
+
+console.log("========= Ficha tecnica de moto =========");
+console.log("Marca:", marca);
+console.log("Cilindrada:", cilindrada, "cc");
+console.log("Esta encendida:", estaEncendida);
+console.log("Kilometraje:", kilometraje, "km");
+console.log("Proximo mantenimiento:"+ (proximoMantenimientoKm ? proximoMantenimientoKm : "No especificado"));
+console.log("Necesita mantenimiento:", necesitaMantenimiento);
+console.log("=========================================");
