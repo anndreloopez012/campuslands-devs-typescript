84136ac73673fab9ef028b88654db77796709fe1
DeniseLH1 <jdlhprincesa@gmail.com>
2026-06-07 22:52:21 -0600

    feat: solucion exercicio 01 introduction a typescript

--- ejercicios/01-introduccion-typescript/index.ts
+++ ejercicios/01-introduccion-typescript/index.ts
@@ -1,8 +1,11 @@
 export {};
-
 // Ejercicio 1: Introduccion a TypeScript
-// Contexto: Videojuegos Shooter
+const nombreJugador: string = "Diegojota";
+let armaPrincipal: string = "Arco de fuego";
+let municionDisponible: number = 5;
+let tieneEscudoActivo: boolean = true;
+const mapaActual: string = "Base Alpha";
+const modoJuego: string = "Capturar la bandera";
 
-// TODO: declara las variables tipadas del jugador.
-
-// TODO: imprime un resumen usando console.log.
+console.log("=== ESTADO DEL JUGADOR ===");
+console.log(`El jugador ${nombreJugador} cuenta con un arma principal: ${armaPrincipal}, con una munición de: ${municionDisponible} flechas y ${tieneEscudoActivo ? "Sí" : "No"} tiene el escudo activo; te encuentras en el mapa: ${mapaActual} y el modo de juego es: ${modoJuego}`);
