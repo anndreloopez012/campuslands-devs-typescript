d7568368455720710e661c9b20e176964d32ed66
DeniseLH1 <jdlhprincesa@gmail.com>
2026-06-08 00:05:54 -0600

    feat: solucion ejercicio 02 tipos primitivos

--- ejercicios/02-tipos-primitivos/JenniferLopez.ts
+++ ejercicios/02-tipos-primitivos/JenniferLopez.ts
@@ -1,8 +1,24 @@
 export {};
 
 // Ejercicio 2: Tipos primitivos
-// Contexto: Motos
 
-// TODO: declara datos primitivos para una moto.
+const marca: string = "Yamaha";
+const cilindrada: number = 300;
+const estaEncendida: boolean = true;
+const kilometraje: number = 3500;
 
-// TODO: calcula si necesita mantenimiento.
+let proximoMantenimiento: null | undefined = undefined;
+console.log("Ficha Tecnica");
+console.log("Marca:", marca);
+console.log("Cilindrada:", cilindrada);
+console.log("Esta Encendida:", estaEncendida);
+console.log("Kilometraje:", kilometraje);
+console.log("Proximo Mantenimiento:", proximoMantenimiento);
+proximoMantenimiento = null;
+const necesitaMantenimiento: boolean = kilometraje > 3000;
+if (necesitaMantenimiento) {
+    console.log("Aviso: La moto necesita mantenimiento.");
+} else {
+    console.log("Aviso: La moto no necesita mantenimiento.");
+    
+}
