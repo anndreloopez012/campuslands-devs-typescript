14420822d843ae043d910f303c4353836daf2e03
Anderson-Oloroso <henrikog4@gmail.com>
2026-06-07 21:19:25 -0600

    :sparkels: Feat(ejercicio-08): Ejercicio Union Types terminado

--- ejercicios/08-union-types/index.ts
+++ ejercicios/08-union-types/index.ts
@@ -4,3 +4,41 @@ export {};
 // Contexto: Vehiculos deportivos e hiperdeportivos
 
 // TODO: crea una union discriminada para vehiculos.
+
+type autoDeportivo = {
+  tipo: "deportivo";
+  marca: string;
+  modelo: string;
+  caballosFuerza: number;
+  tipoTraccion: "trasera" | "delantera" | "integral";
+};
+
+type superDeportivo = {
+  tipo: "hiperdeportivo";
+  marca: string;
+  modelo: string;
+  caballosFuerza: number; // en caballos de fuerza
+  velocidadMaxima: number; // en km/h
+  unidadesProducidas: number;
+};
+
+type autosAura = autoDeportivo | superDeportivo;
+const porsche911: autosAura = {
+  tipo: "deportivo",
+  marca: "Porsche",
+  modelo: "911 Carrera S",
+  caballosFuerza: 450,
+  tipoTraccion: "trasera",
+};
+
+const bugattiChiron: autosAura = {
+  tipo: "hiperdeportivo",
+  marca: "Bugatti",
+  modelo: "Chiron",
+  caballosFuerza: 1500,
+  velocidadMaxima: 420,
+  unidadesProducidas: 500,
+};
+
+console.log(porsche911);
+console.log(bugattiChiron);
