357e0839d5a978e7922186a3ae35380b0b6333cf
danielAVAR <danielaguilar5502@gmail.com>
2026-06-08 23:56:05 -0600

    feat: ejercicio 1

---
+++ ejercicios/01-introduccion-typescript/ts1.ts
@@ -0,0 +1,16 @@
+export {};
+
+// Ejercicio 1: Introduccion a TypeScript
+// Contexto: Videojuegos Shooter
+// TODO: declara las variables tipadas del jugador.
+const usuario: string = "Jugador1";
+let arma_principal: string = "Rifle de asalto";
+let municion: number = 120;
+let escudo: boolean = true;
+
+
+// TODO: imprime un resumen usando console.log.
+console.log(`Usuario: ${usuario}`);
+console.log(`Arma Principal: ${arma_principal}`);
+console.log(`Munición: ${municion}`);
+console.log(`Escudo: ${escudo}`);
