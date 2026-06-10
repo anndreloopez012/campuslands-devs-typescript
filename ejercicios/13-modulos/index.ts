d6b507bea0660f7ca359be42ee0042ee06817bb1
Anderson-Oloroso <henrikog4@gmail.com>
2026-06-09 20:23:25 -0600

    :sparkles: Feat(ejercicio-13): Ejercicio de Módulos terminado

--- ejercicios/13-modulos/index.ts
+++ ejercicios/13-modulos/index.ts
@@ -1,5 +1,5 @@
 import destinos from "./datos.json" with { type: "json" };
-import { filtrarPorPresupuesto, formatearDestino } from "./utilidades";
+import { filtrarPorPresupuesto, formatearDestino } from "./utilidades.ts";
 
 // Ejercicio 13: Modulos
 // Contexto: Aplicacion de viajes
@@ -8,3 +8,13 @@ import { filtrarPorPresupuesto, formatearDestino } from "./utilidades";
 console.log(destinos);
 console.log(filtrarPorPresupuesto);
 console.log(formatearDestino);
+
+console.log("=== Lista de Destinos Completa ===");
+console.log(JSON.stringify(destinos, null, 2));
+
+const misDestinosFiltrados = filtrarPorPresupuesto(destinos, 500);
+
+console.log("\n=== Destinos ===");
+misDestinosFiltrados.forEach((destino) => {
+  console.log(formatearDestino(destino));
+});
