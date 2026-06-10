7adc9391be467e8d955b9491e6749bcf9a1bc6f2
barriosevelyn488-web <barrios.evelyn.488@gmail.com>
2026-06-10 11:08:10 -0600

    feat: resolución ejercicio 11 de typescript

---
+++ resoluciones/evelyn-barrios/ejercicio11/index.ts
@@ -0,0 +1,34 @@
+export {};
+
+
+// Ejercicio 11: Clases
+// Contexto: Sistema de motocicletas
+
+// TODO: crea la clase Motocicleta.
+
+class Motocicleta {
+ protected encendida: boolean = false;
+
+
+ constructor(
+   public readonly marca: string,
+   public modelo: string,
+ ) {}
+
+
+ conmutarMotor(): void {
+   this.encendida = !this.encendida;
+   console.log(
+     `La ${this.marca} ahora está ${this.encendida ? "encendida" : "apagada"}.`,
+   );
+ }
+
+
+ public verEstado(): string {
+   return this.encendida ? "En marcha" : "Detenida";
+ }
+}
+
+
+const miMoto = new Motocicleta("Yamaha", "MT-07");
+miMoto.conmutarMotor();
