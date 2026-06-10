d7b78ad01a83c0ab9400741da9f42b62a7a31c54
Anderson-Oloroso <henrikog4@gmail.com>
2026-06-10 06:16:30 -0600

    :recycle: Refactor: Restaurando los archivos de los ejercicios

--- ejercicios/04-interfaces/index.ts
+++ ejercicios/04-interfaces/index.ts
@@ -5,33 +5,3 @@ export {};
 
 // TODO: define la interfaz PersonajeRPG.
 // TODO: crea un array de personajes.
-
-console.log("=========== Interfaces ===========");
-
-interface personajeRPG {
-  nombre: string;
-  clase: "campesino" | "mago" | "guerrero";
-  nivel: number;
-  puntosDeVida: number;
-}
-
-const personajes: personajeRPG[] = [
-  {
-    nombre: "Aragorn",
-    clase: "guerrero",
-    nivel: 20,
-    puntosDeVida: 150,
-  },
-  {
-    nombre: "Gandalf",
-    clase: "mago",
-    nivel: 30,
-    puntosDeVida: 100,
-  },
-  {
-    nombre: "Frodo",
-    clase: "campesino",
-    nivel: 5,
-    puntosDeVida: 50,
-  },
-];
--- ejercicios/13-modulos/index.ts
+++ ejercicios/13-modulos/index.ts
@@ -1,5 +1,5 @@
 import destinos from "./datos.json" with { type: "json" };
-import { filtrarPorPresupuesto, formatearDestino } from "./utilidades.ts";
+import { filtrarPorPresupuesto, formatearDestino } from "./utilidades";
 
 // Ejercicio 13: Modulos
 // Contexto: Aplicacion de viajes
@@ -8,13 +8,3 @@ import { filtrarPorPresupuesto, formatearDestino } from "./utilidades.ts";
 console.log(destinos);
 console.log(filtrarPorPresupuesto);
 console.log(formatearDestino);
-
-console.log("=== Lista de Destinos Completa ===");
-console.log(JSON.stringify(destinos, null, 2));
-
-const misDestinosFiltrados = filtrarPorPresupuesto(destinos, 500);
-
-console.log("\n=== Destinos ===");
-misDestinosFiltrados.forEach((destino) => {
-  console.log(formatearDestino(destino));
-});
