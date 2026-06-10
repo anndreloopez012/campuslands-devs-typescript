72eb46b2793b041c3de4a62c6d41e925cc096fac
lemical07 <icaleladio@gmail.com>
2026-06-09 21:57:29 -0600

    feat: separar responsabilidades usando `export` e `import

--- ejercicios/13-modulos/resoluciones/selvin-lem/ejericio-13/index.ts
+++ ejercicios/13-modulos/resoluciones/selvin-lem/ejericio-13/index.ts
@@ -1,10 +1,27 @@
 import destinos from "./datos.json" with { type: "json" };
-import { filtrarPorPresupuesto, formatearDestino } from "./utilidades";
+import { Destino, filtrarPorPresupuesto, formatearDestino } from "./utilidades.js";
+
+
 
 // Ejercicio 13: Modulos
 // Contexto: Aplicacion de viajes
 
-// TODO: usa las utilidades importadas para filtrar y mostrar destinos.
-console.log(destinos);
-console.log(filtrarPorPresupuesto);
-console.log(formatearDestino);
+const TOPE_PRESUPUESTO_VIAJE = 600; 
+
+console.log("=====================SISTEMA DE GESTIÓN DE VIAJES ====================");
+console.log(`Buscando itinerarios con un presupuesto máximo de: USD ${TOPE_PRESUPUESTO_VIAJE}`);
+console.log("---------------------------------------------------------------------\n");
+const opcionesValidas: Destino[] = filtrarPorPresupuesto(destinos, TOPE_PRESUPUESTO_VIAJE);
+
+if (opcionesValidas.length > 0) {
+    console.log(" DESTINOS SELECCIONADOS PARA SU VIAJE:");
+    console.log("---------------------------------------------------------------------");
+    opcionesValidas.forEach((lugar: Destino) => {
+        const lineaInformativa = formatearDestino(lugar);
+        console.log(`• ${lineaInformativa}`);
+    });
+} else {
+    console.log(" Operación Terminada: Ningún destino del catálogo se ajusta al presupuesto indicado.");
+}
+
+console.log("\n=====================================================================");
