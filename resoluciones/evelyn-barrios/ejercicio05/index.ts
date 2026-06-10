9001c89348694aecd34d81f5be5538120e9b6bb5
barriosevelyn488-web <barrios.evelyn.488@gmail.com>
2026-06-10 09:55:12 -0600

    resolución del ejercicio 05 typescript

---
+++ resoluciones/evelyn-barrios/ejercicio05/index.ts
@@ -0,0 +1,26 @@
+export {};
+
+
+type Talla = "S" | "M" | "L" | "XL";
+type CategoriaRopa = "Deportiva" | "Casual" | "Formal";
+
+
+type ProductoRopa = {
+ readonly id: string;
+ nombre: string;
+ talla: Talla;
+ categoria: CategoriaRopa;
+ precio: number;
+};
+
+
+const prendaNueva: ProductoRopa = {
+ id: "ROP-500",
+ nombre: "Camiseta Tech",
+ talla: "M",
+ categoria: "Deportiva",
+ precio: 29.99,
+};
+
+
+console.log(`Producto: ${prendaNueva.nombre} - Precio: $${prendaNueva.precio}`);
