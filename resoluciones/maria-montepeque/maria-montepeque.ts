87c457965291b9805f104d08992ecb9064e96b67
MariaJoseMontepequeZet <montepequemajo@gmail.com>
2026-06-10 22:36:27 -0600

    Resolver ejercicio 11 TypeScript

---
+++ resoluciones/maria-montepeque/maria-montepeque.ts
@@ -0,0 +1,42 @@
+export {};
+
+// Ejercicio 11: Clases
+
+class Motocicleta {
+  private kilometraje: number;
+  private encendida = false;
+
+  constructor(
+    public readonly marca: string,
+    public readonly modelo: string,
+    kilometrajeInicial: number,
+  ) {
+    this.kilometraje = kilometrajeInicial;
+  }
+
+  encender(): void {
+    this.encendida = true;
+  }
+
+  recorrer(kilometros: number): void {
+    if (!this.encendida)
+      throw new Error(
+        "La motocicleta debe estar encendida para recorrer kilometros.",
+      );
+    this.kilometraje += kilometros;
+  }
+
+  get km(): number {
+    return this.kilometraje;
+  }
+
+  obtenerEstado(): string {
+    return `${this.marca} ${this.modelo} - ${this.kilometraje} km - ${this.encendida ? "encendida" : "apagada"}`;
+  }
+}
+
+const moto = new Motocicleta("Kawasaki", "Ninja 400", 1200);
+moto.encender();
+moto.recorrer(35);
+console.log(moto.obtenerEstado());
+console.log("Kilometraje consultado:", moto.km);
